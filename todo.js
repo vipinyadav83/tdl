216b6d20f28a07d4357df418c28fd1bc6cbc9c49
“vipin <vkyadav8383@gmail.com>
2024-03-10 15:26:36 +0530

    html css js

---
+++ todo.js
@@ -0,0 +1,24 @@
+let inputs = document.querySelector("#inp");
+let txt = document.querySelector(".text");
+
+function Add(){
+    if(inputs.value ==""){
+        alert("please enter the text");
+    }
+    else{
+           let newEle = document.createElement('ul');
+           newEle.innerHTML = `${inputs.value} <i class="bi bi-archive-fill" id="del"></i>  <i class="bi bi-pencil-square" id="edit"></i>`;
+           inputs.value ="";
+           txt.appendChild(newEle);
+            newEle.querySelector( '#del').addEventListener('click',Delete);
+           function Delete(){
+              newEle.remove()
+           }
+          newEle.querySelector('#edit').addEventListener('click',edt);
+           function edt(){
+             let edlist = prompt("edit the list");
+             newEle.innerHTML = `${edlist}`;
+
+           }
+    }
+}
